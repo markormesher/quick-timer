5dfaa708bab46173e69c5805eb0a2788b7147e05
Mark Ormesher <me@markormesher.co.uk>
2017-05-29 10:23:22 +0100

    Empty Kotlin Android app

---
+++ app/src/main/kotlin/uk/co/markormesher/resttimer/MainActivity.kt
@@ -0,0 +1,12 @@
+package uk.co.markormesher.resttimer
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+		super.onCreate(savedInstanceState, persistentState)
+	}
+}
